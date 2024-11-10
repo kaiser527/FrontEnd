29ecbb40329e16392ec07430160a91d92e5ec9a8
kaiser527 <164615999+kaiser527@users.noreply.github.com>
2024-11-10 08:42:30 +0700

    Update TableUsers.jsx

--- src/Components/TableUsers.jsx
+++ src/Components/TableUsers.jsx
@@ -1,26 +1,4 @@
 const TableUsers = () => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>No</th>
-          <th>Email</th>
-          <th>Name</th>
-          <th>City</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Peter</td>
-          <td>Griffin</td>
-          <td>Griffin</td>
-          <td>Griffin</td>
-          <td>$100</td>
-        </tr>
-      </tbody>
-    </table>
-  );
-};
+  return <div>TableUsers</div>
 
 export default TableUsers;
